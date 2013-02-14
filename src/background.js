a5f036f27e51d61475bd6a9c39d15697f12f2580
ruche7 <pg@ruche-home.net>
2013-02-14 23:45:21 +0900

    ・スクリーンではなくカレントウィンドウの中央にポップアップ表示するように変更。 ・window.open ではなく chrome.windows.create を使うようにした。（マルチディスプレイ対応）

--- src/background.js
+++ src/background.js
@@ -1,14 +1,24 @@
 function onClicked(tab) {
-  var url = 'https://twitter.com/intent/tweet?'
-    + 'text=' + encodeURIComponent(tab.title)
-    + '&url=' + encodeURIComponent(tab.url);
-  var w = 640;
-  var h = 360;
-  var x = (screen.width - w) / 2;
-  var y = (screen.height - h) / 2;
-  window.open(url, null,
-      'left='+x+',top='+y+',width='+w+',height='+h
-      +',status=no');
+  chrome.windows.get(tab.windowId, function(wnd) {
+    var url =
+      'https://twitter.com/intent/tweet?' +
+      'text=' + encodeURIComponent(tab.title) +
+      '&url=' + encodeURIComponent(tab.url);
+    var w = 640;
+    var h = 360;
+    var x = wnd.left + (wnd.width - w) / 2;
+    var y = wnd.top + (wnd.height - h) / 2;
+
+    var param = {
+      'url'    : url,
+      'left'   : x,
+      'top'    : y,
+      'width'  : w,
+      'height' : h,
+      'type'   : 'popup',
+    };
+    chrome.windows.create(param);
+  });
 }
 
 chrome.browserAction.onClicked.addListener(onClicked);
